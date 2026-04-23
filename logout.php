f47d35635c6ebc8867eede97137ffbd00c4810e4
Nico Schubert <ns.schubertmedia@googlemail.com>
2026-04-23 18:54:30 +0200

    fix(logout): BUG-008 Logout nur per POST mit CSRF
    Logout-Link im Header zeigt nun das Bestaetigungsformular (GET).
    Der eigentliche Logout passiert per POST mit CSRF-Token-Check.
    GET /logout.php?logout=1 loggt den Nutzer NICHT mehr aus, sondern
    zeigt ebenfalls nur das Formular. Dadurch laesst sich ein Nutzer
    nicht mehr via <img src> oder sonstigen GET-basierten Tricks
    ungewollt ausloggen.

--- logout.php
+++ logout.php
@@ -10,19 +10,30 @@ declare(strict_types=1);
  * Copyright (c) 2024 PowerScripts
  */
 
+use PowerPHPBoard\CSRF;
 use PowerPHPBoard\Security;
 use PowerPHPBoard\Session;
 
 // Load configuration first (includes autoloader)
 require_once __DIR__ . '/config.inc.php';
 
-// Get logout parameter
-$logout = Security::getInt('logout');
+Session::start();
 
-// Process logout before header if requested
+$logout = 0;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $logout = Security::getInt('logout', 'POST');
+}
+
+$catid = Security::getInt('catid');
+$boardid = Security::getInt('boardid');
+
+// BUG-008: Perform logout only on POST with valid CSRF token
+$csrfOk = false;
 if ($logout === 1) {
-    Session::start();
-    Session::logout();
+    $csrfOk = CSRF::validateFromPost();
+    if ($csrfOk) {
+        Session::logout();
+    }
 }
 
 include __DIR__ . '/header.inc.php';
@@ -31,7 +42,7 @@ include __DIR__ . '/header.inc.php';
 <table border="0" cellpadding="2" cellspacing="1" width="100%">
 
 <?php
-if ($logout === 1) {
+if ($logout === 1 && $csrfOk) {
     echo '
       <tr><td bgcolor="' . Security::escape($settings['tablebg3'] ?? '#cccccc') . '">
       <b>' . ($lang_statusmessage ?? 'Status') . '</b>
@@ -43,7 +54,20 @@ if ($logout === 1) {
       <a href="index.php">Home</a>
       </td></tr>
     ';
+} elseif ($logout === 1 && !$csrfOk) {
+    echo '
+      <tr><td bgcolor="' . Security::escape($settings['tablebg3'] ?? '#cccccc') . '">
+      <b>' . ($lang_errormessage ?? 'Error') . '</b>
+      </td></tr>
+      <tr><td bgcolor="' . Security::escape($settings['tablebg2'] ?? '#eeeeee') . '"><br>
+      Security token invalid. Please try again.<br><br>
+      </td></tr>
+      <tr><td bgcolor="' . Security::escape($settings['tablebg1'] ?? '#ffffff') . '" align="center">
+      <a href="logout.php">' . ($lang_back ?? 'Back') . '</a>
+      </td></tr>
+    ';
 } else {
+    // Show confirmation form (POST-only logout)
     echo '
       <tr><td bgcolor="' . Security::escape($settings['tablebg3'] ?? '#cccccc') . '">
       <b>' . ($lang_logout ?? 'Logout') . '</b>
@@ -52,7 +76,14 @@ if ($logout === 1) {
       ' . ($lang_reallylogout ?? 'Do you really want to logout?') . '<br><br>
       </td></tr>
       <tr><td bgcolor="' . Security::escape($settings['tablebg1'] ?? '#ffffff') . '" align="center">
-      <a href="logout.php?logout=1&catid=' . (int) $catid . '&boardid=' . (int) $boardid . '">' . ($lang_yeslogout ?? 'Yes, logout') . '</a> |
+      <form action="logout.php" method="post" style="display:inline">
+      ' . CSRF::getTokenField() . '
+      <input type="hidden" name="logout" value="1">
+      <input type="hidden" name="catid" value="' . (int) $catid . '">
+      <input type="hidden" name="boardid" value="' . (int) $boardid . '">
+      <button type="submit">' . ($lang_yeslogout ?? 'Yes, logout') . '</button>
+      </form>
+      &nbsp;|&nbsp;
       <a href="index.php?catid=' . (int) $catid . '&boardid=' . (int) $boardid . '">' . ($lang_nologout ?? 'No, stay logged in') . '</a>
       </td></tr>
     ';
